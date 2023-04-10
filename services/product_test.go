8b21d3f819bfdd47f9ae1ca1bc5ffdb37c94d3cc
I Gusti Agung Anom Semara Putra <agoenkanom1305@gmail.com>
2023-04-10 10:22:02 +0800

    test get all

--- services/product_test.go
+++ services/product_test.go
@@ -32,17 +32,76 @@ func TestProductServiceGetOneProductFound(t *testing.T){
 	Description: "Desc", 
 	UserID : 1,
 	}
-
 	productRepository.Mock.On("FindByID", 2).Return(data)
-
 	product, err := productService.GetOneProduct(2)
-
-
-
 	assert.Nil(t, err)
 	assert.NotNil(t, product)
 	assert.Equal(t, data.ID, product.ID)
 	assert.Equal(t, data.Title, product.Title)
 	assert.Equal(t, data.Description, product.Description)
 	assert.Equal(t, data.UserID, product.UserID)
+}
+
+func TestProductServiceGetAllProductFound(t *testing.T){
+	data := []entity.Product{
+		{	
+		ID: 2,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 2,
+		},
+		{	
+		ID: 3,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 3,
+		},
+		{	
+		ID: 4,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 4,
+		},
+	}
+	for i, x := range data {
+	productRepository.Mock.On("FindByID", i+3).Return(x)
+	product, err := productService.GetOneProduct(i+3)
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.Equal(t, x.ID, product.ID)
+	assert.Equal(t, x.Title, product.Title)
+	assert.Equal(t, x.Description, product.Description)
+	assert.Equal(t, x.UserID, product.UserID)
+	}
+}
+
+
+func TestProductServiceGetAllProductNotFound(t *testing.T){
+	data := []entity.Product{
+		{	
+		ID: 2,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 2,
+		},
+		{	
+		ID: 3,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 3,
+		},
+		{	
+		ID: 4,
+		Title: "Title",
+		Description: "Desc", 
+		UserID : 4,
+		},
+	}
+	for i, _ := range data {
+	productRepository.Mock.On("FindByID", i+6).Return(nil)
+	product, err := productService.GetOneProduct(i+6)
+	assert.Nil(t, product)
+	assert.NotNil(t, err)
+	assert.Equal(t, "Product not Found", err.Error(), "Error response has to be 'Product not Found'")
+	}
 }
